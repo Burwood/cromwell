82bf1c63d32cfb89a11ba06f714e76b3c85d54aa
Alexis Hernandez <alexis22229@gmail.com>
2023-05-18 22:48:04 -0700

    GCP Batch: Add LocalizationSpec

--- supportedBackends/google/batch/src/main/scala/cromwell/backend/google/batch/runnable/Localization.scala
+++ supportedBackends/google/batch/src/main/scala/cromwell/backend/google/batch/runnable/Localization.scala
@@ -76,7 +76,7 @@ trait Localization {
 object Localization {
 
   // TODO: Avoid loading the global config because Cromwell already loaded it
-  //private lazy val config = ConfigFactory.load
+  private lazy val config = ConfigFactory.load
 
   def drsRunnable(manifestPath: Path,
                 labels: Map[String, String],
@@ -84,16 +84,15 @@ object Localization {
                ): Runnable.Builder = {
     import RunnableBuilder.EnhancedRunnableBuilder
 
-    val config = ConfigFactory.load
-    //val drsResolverConfig = config.getConfig("filesystems.drs.global.config.resolver")
-    //val drsConfig = DrsConfig.fromConfig(drsResolverConfig)
+//    val drsResolverConfig = config.getConfig("filesystems.drs.global.config.resolver")
+//    val drsConfig = DrsConfig.fromConfig(drsResolverConfig)
     val drsDockerImage = config.getString("drs.localization.docker-image")
 
     val manifestArg = List("-m", manifestPath.pathAsString)
     val requesterPaysArg = requesterPaysProjectId.map(r => List("-r", r)).getOrElse(List.empty)
     val drsCommand = manifestArg ++ requesterPaysArg
 
-    //val drsResolverEnv = DrsConfig.toEnv(drsConfig)
+//    val drsResolverEnv = DrsConfig.toEnv(drsConfig)
 
     RunnableBuilder
       .withImage(drsDockerImage)
